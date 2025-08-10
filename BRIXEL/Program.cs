3854cd195755f9e901c2843d14aeedb09e5bb875
Amer Hatem <amerhatem01@gmail.com>
2025-08-10 20:26:17 +0300

    Update Program.cs

--- BRIXEL/Program.cs
+++ BRIXEL/Program.cs
@@ -89,17 +89,19 @@ namespace BRIXEL
             {
                 options.AddPolicy("AllowFrontend", policy =>
                 {
-                    policy.WithOrigins(
-                        "http://localhost:5173",
-                        "http://localhost:5000",
-                        "http://localhost:8080",
-                "https://brixel-ps.netlify.app/"
-                    )
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
+                    policy
+                        .WithOrigins(
+                            "http://localhost:5173",
+                            "http://localhost:5000",
+                            "http://localhost:8080",
+                            "https://brixel-ps.netlify.app"  
+                        )
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+              
                 });
             });
+
 
             // -------- 6) رفع حجم الملفات (للصور) --------
             builder.Services.Configure<FormOptions>(o =>
